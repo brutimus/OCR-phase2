36ed8c5940eab1a1bbc5ec47789561b920a8b9b3
Sean Stoops <brutimus@gmail.com>
2014-01-30 18:26:53 -0800

    Fixing up search and share tools.

--- js/app/main.js
+++ js/app/main.js
@@ -6,8 +6,12 @@ define([
     tooltip('nav .tools a', 'nav .tools ul');
 
     // Menu for the navbar search button
-	tooltip('nav .search a', 'nav .search span');
+	tooltip('nav .search a', 'nav .search .input-group');
 
 	// Menu for the newstogram widget help button
 	tooltip('.newstogram .help-button', '.newstogram .help');
+
+	// Menu for article share tools more button
+	tooltip('.share-tools:nth-of-type(1) .more > a', '.share-tools:nth-of-type(1) .more  ul');
+	tooltip('.share-tools:nth-of-type(2) .more > a', '.share-tools:nth-of-type(2) .more  ul');
 });
