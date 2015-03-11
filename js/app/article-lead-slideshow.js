1f42f8115be4f53ebb1f06102fb5ea8759411ead
Sean Stoops <brutimus@gmail.com>
2015-03-10 19:43:58 -0700

    Checkin

--- js/app/article-lead-slideshow.js
+++ js/app/article-lead-slideshow.js
@@ -12,51 +12,13 @@ define(['jquery', 'jquery.fancybox', 'jquery.slides', 'domReady!'], function(){
         afterShow: function(){
             $('.slideshows-wrapper').addClass('fullscreen');
             $('.lead-slideshow').slick('setPosition');
+            $('body').css('overflow', 'hidden');
         },
         afterClose: function(){
             $(this.href).removeClass('fullscreen');
             $(this.href).show();
             $('.lead-slideshow').slick('setPosition');
+            $('body').css('overflow', 'auto');
         }
     });
-    // $('article#story .slideshows-wrapper .fullscreen a').click(function(){
-    //     var wrapper = $('.slideshows-wrapper');
-    //     function exitFullScreen(){
-    //         if (document.exitFullscreen) {
-    //             document.exitFullscreen();
-    //         } else if (document.msExitFullscreen) {
-    //             document.msExitFullscreen();
-    //         } else if (document.mozCancelFullScreen) {
-    //             document.mozCancelFullScreen();
-    //         } else if (document.webkitExitFullscreen) {
-    //             document.webkitExitFullscreen();
-    //         }
-    //     }
-    //     function toggleFullScreen(el) {
-    //         if (!document.fullscreenElement &&    // alternative standard method
-    //             !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
-    //             if (el.requestFullscreen) {
-    //                 el.requestFullscreen();
-    //             } else if (el.msRequestFullscreen) {
-    //                 el.msRequestFullscreen();
-    //             } else if (el.mozRequestFullScreen) {
-    //                 el.mozRequestFullScreen();
-    //             } else if (el.webkitRequestFullscreen) {
-    //                 el.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-    //             }
-    //         } else {
-    //             exitFullScreen()
-    //         }
-    //     }
-    //     // if (wrapper.hasClass('fullscreen')) {
-    //     //     wrapper.removeClass('fullscreen')
-    //     // } else {
-    //     //     wrapper.addClass('fullscreen')[0].webkitRequestFullscreen()
-    //     // }
-    //     toggleFullScreen(wrapper[0])
-    //     $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(){
-    //         wrapper.toggleClass('fullscreen');
-    //     });
-    //     $('.close-button').click(exitFullScreen);
-    // });
 });
